ac3dfd0980100a7a963e393310fd601e129d5b1c
yang.tian <yang.tian@e7cb0379-d053-0e4b-a1ac-43c7dbffadbe>
2012-03-29 07:54:15 +0000

    TianYang: change server url
    git-svn-id: https://source.kii.com/servo/platform/sdk-mobile-client/androidV1/Samples/KiiBoard@21903 e7cb0379-d053-0e4b-a1ac-43c7dbffadbe

--- src/com/kii/cloud/board/sdk/KiiBoardClient.java
+++ src/com/kii/cloud/board/sdk/KiiBoardClient.java
@@ -20,7 +20,7 @@ public class KiiBoardClient {
     private static final String UPDATE_TIME = "update_time";
     private static final String EMAIL = "email";
 
-    private static final String DEFAULT_BASE_URL = "http://dev-usergrid.kii.com:12110";
+    private static final String DEFAULT_BASE_URL = "http://api.kii.com:12110";
     private static final String APP_ID = "20df2cb0";
     private static final String APP_KEY = "18cb6a798b6367844ee96e55f3b71f86";
 
