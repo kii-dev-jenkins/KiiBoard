d2931137c48db9beecc4982e6888d738a9205d80
hueymeng.tan <hueymeng.tan@e7cb0379-d053-0e4b-a1ac-43c7dbffadbe>
2012-03-31 06:48:37 +0000

    HM: update the KiiBoard to use KiiCloudStorageSDK build 22073 and update the App ID/Key same as iOS KiiBoard Sample App
    git-svn-id: https://source.kii.com/servo/platform/sdk-mobile-client/androidV1/Samples/KiiBoard@22074 e7cb0379-d053-0e4b-a1ac-43c7dbffadbe

--- src/com/kii/cloud/board/sdk/Constants.java
+++ src/com/kii/cloud/board/sdk/Constants.java
@@ -2,7 +2,7 @@ package com.kii.cloud.board.sdk;
 
 public class Constants {
     public static final String DEFAULT_BASE_URL = "https://api.kii.com/app/api";
-    public static final String APP_ID = "20df2cb0";
-    public static final String APP_KEY = "18cb6a798b6367844ee96e55f3b71f86";
+    public static final String APP_ID = "8b348e3b";
+    public static final String APP_KEY = "61d3ab3c3ccef4c072eead7a281eec60";
     public static final String ACTION_REFRESH = "com.kii.board.refresh";
 }
